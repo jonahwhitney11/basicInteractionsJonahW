6a3cf74457c124c87afd646cdbe834e99921a7a0
jonahwhitney11 <jwhitney@students.mchenry.edu>
2024-01-29 13:11:35 -0600

    Initial Commit

--- basicInteractionsJonahW/ViewController.swift
+++ basicInteractionsJonahW/ViewController.swift
@@ -9,11 +9,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tapLeftLabel: UILabel!
+    
+    @IBOutlet weak var tapRightLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func leftButton(_ sender: Any) {
+        tapLeftLabel.text = "You tapped left"
+    }
+    
+    @IBAction func rightButton(_ sender: Any) {
+        tapRightLabel.text = "You tapped left"
+    }
 }
 
